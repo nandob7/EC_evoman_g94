72ebcdcb804ba5fb2529b6668aa1e4d5cedd37c2
ramondeboer <ramondeboer123@gmail.com>
2024-10-20 23:10:47 +0200

    cleanup

--- tested_best_genomes_plots.py
+++ tested_best_genomes_plots.py
@@ -10,21 +10,10 @@ directory = f'runs/generalist'
 csv_path = os.path.join(directory, f'tested_best_genomes.csv')
 
 df = pd.read_csv(csv_path)
-# Convert the 'gain' column from strings to actual lists using ast.literal_eval
+# Convert the gain from strings to lists
 df['gain'] = df['gain'].apply(ast.literal_eval)
-
 # Replace the list with the sum of the values in the list
 df['gain'] = df['gain'].apply(sum)
-
-# # Flatten the DataFrame: create a new DataFrame for plotting
-# flattened_data = []
-#
-# for index, row in df.iterrows():
-#     for gain in row['gain']:
-#         flattened_data.append({'ea': row['ea'], 'group': row['group'], 'gain': gain})
-#
-# # Create a new DataFrame from the flattened data
-# flattened_df = pd.DataFrame(flattened_data)
 
 # Create a boxplot where the x-axis is the enemy and EA algorithm comparison
 plt.figure(figsize=(10, 6))
